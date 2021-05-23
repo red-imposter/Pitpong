4b8c9fbfbf8e285def29fcba9e6af9a0d34d2768
red-imposter <83532023+red-imposter@users.noreply.github.com>
2021-05-23 11:12:50 +0300

    Update ping_pong.py
    Исправлены ошибки, добавлена логика мяча

--- ping_pong.py
+++ ping_pong.py
@@ -1,10 +1,10 @@
 from pygame import *
-window = display.set_mode((500,500))
+window = display.set_mode((950,700))
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,img,x,y,speed):
+    def __init__(self,img,x,y,speed,width,height):
         super().__init__()
-        self.image = transform.scale(image.load(img),(50,50))
+        self.image = transform.scale(image.load(img),(width,height))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -13,45 +13,46 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image,(self.rect.x,self.rect.y))
 class racketka(GameSprite):
     def update1(self):
-        keys = key.get_pressed()
+        keys_pressed = key.get_pressed()
         if keys_pressed[K_w]:
             if self.rect.y >0:
-                self.rect.y -= 1
+                self.rect.y -= self.speed
         if keys_pressed[K_s]:
-            if self.rect.y <500:
-                self.rect.y += 1
+            if self.rect.y < 600:
+                self.rect.y += self.speed
     def update2(self):
-        keys = key.get_pressed()
+        keys_pressed = key.get_pressed()
         if keys_pressed[K_UP]:
             if self.rect.y >0:
-                self.rect.y -= 1
+                self.rect.y -= self.speed
         if keys_pressed[K_DOWN]:
-            if self.rect.y <500:
-                self.rect.y += 1
+            if self.rect.y < 600:
+                self.rect.y += self.speed
 class Ball(GameSprite):
-    def __init__(self,img,x,y,speed_x,speed_y):
-        super().__init__(img,x,y,0,speed_x,speed_y)
+    def __init__(self,img,x,y,speed_x,speed_y,width,height):
+        super().__init__(img,x,y,0,width,height)
         self.speed_x = speed_x
-        self.speed.y = speed.y
+        self.speed_y = speed_y
     def update_position(self):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
     def update_speeds(self,speed_x,speed_y):
-        self.speed.x += self.speed_x
-        self.speed.y += self.speed_y
-#if sprite.collide_rect(racketka1,ball):
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
 run = True
 clock = time.Clock()
 FPS = 60
 
-finish = False
-back = (23, 11, 186)
-window.fill(back)
+speed_x = 6
+speed_y = 6
 
-racketka1 = racketka('racket.png',50,250,4)
-racketka2 = racketka('racket.png',450,250,4)
-ball = Ball('tenis_ball.png',250,250,3,3,)
+finish = False
+
+
+racketka1 = racketka('racket.png',5,250,4,50,100)
+racketka2 = racketka('racket.png',895,250,4,50,100)
+ball = Ball('tenis_ball.png',250,250,1,1,50,50)
 
 
 while run:
@@ -59,10 +60,19 @@ while run:
         if e.type == QUIT:
             run = False
     if finish != True:
+        back = (23, 11, 186)
+        window.fill(back)
         racketka1.draw()
-        racketka2.draw()
+        racketka2.draw() 
+        ball.draw()
         racketka1.update1()
         racketka2.update2()
+        ball.update_speeds(speed_x,speed_y)
+        ball.update_position()
+        if sprite.collide_rect(racketka1,ball) or sprite.collide_rect(racketka2,ball):
+            speed_x *= -1
+        if ball.rect.y > 650 or ball.rect.y < 0:
+            speed_y *= -1
 
         display.update()
         clock.tick(FPS)
